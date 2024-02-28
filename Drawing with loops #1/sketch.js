eb2e9d14f83205c0e8937daa8f3df0dfe33ba35d
ShamsSPSD <233007shams@gmail.com>
2024-02-28 15:20:20 -0600

    loop prac
    Loop practice

--- Drawing with loops #1/sketch.js
+++ Drawing with loops #1/sketch.js
@@ -1,36 +1,71 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Drawing with Loops 1
+// Omar shams
+// February 28, 2024
+// Using Loops + Arrays to draw
 
-let Width = 400
-let Height = 300
-let xPos, yPos;
+//Global Variables
+let xPos, yPos;   //declaration only...
 
 function setup() {
-  createCanvas(Width, Height);
+  createCanvas(400 , 400);
+  xPos = [];  yPos = [];
+  initWithLoops(); //comment out for the on-your-own challenge
+  // xPos = [width*0.05, width*0.95,width*0.05,width*0.95];
+  // yPos = [height*0.05,height*0.05,height*0.95,height*0.95];
+}
+
+function initWithLoops(){
+  //lay down some initial circles, but using loops to do so.
+  for(let x = 10; x<width; x+=20){
+    xPos.push(x); 
+    yPos.push(height);
+    xPos.push(x); 
+    yPos.push(height/600);
+  }
+  for(let x = 10; x<height; x+=20){
+    xPos.push(height); 
+    yPos.push(x);
+    xPos.push(height/600); 
+    yPos.push(x);
+  }
 }
 
 function draw() {
   background(220);
-  cornersandmouse()
+  cornersAndMouseLoop();
 }
 
 function mousePressed(){
-  xPos.push(mouseX)
-  yPos.push(mouseY)
+  //this calls automatically on a mousePress
+  xPos.push(mouseX);
+  yPos.push(mouseY);
 }
 
-function cornersandmouse(){
-  circle(Width * 0.05, Height * 0.05, 10)
-  circle(Width * 0.95, Height * 0.95, 10)
-  circle(Width * 0.05, Height * 0.95, 10)
-  circle(Width * 0.95, Height * 0.05, 10)
-  circle(mouseX,mouseY, 10)
-  line(Width * 0.05, Height * 0.05, mouseX,mouseY)
-  line(Width * 0.95, Height * 0.95, mouseX,mouseY)
-  line(Width * 0.05, Height * 0.95, mouseX,mouseY)
-  line(Width * 0.95, Height * 0.05, mouseX,mouseY)
+function cornersAndMouseLoop(){
+  // a hopefully slightly more elegant version...
+  let i = 0;
+  while(i < xPos.length){
+    let x = xPos[i];
+    let y = yPos[i];
+    circle(x,y,20);
+    line(x,y,mouseX,mouseY);
+    i++; 
+  }
+  circle(mouseX, mouseY, 20);
+}
+
+function cornersAndMouse(){
+  // draw some circles near each of the four corners
+  // and connect some lines from there to the mouse position
+  fill(255);
+  circle(width*0.05, height*0.05, 20);
+  circle(width*0.95, height*0.05, 20);
+  circle(width*0.05, height*0.95, 20);
+  circle(width*0.95, height*0.95, 20);
+  circle(mouseX, mouseY, 20);
+  //lines
+  line(width*0.05, height*0.05, mouseX,mouseY);
+  line(width*0.95, height*0.05,  mouseX,mouseY);
+  line(width*0.05, height*0.95,  mouseX,mouseY);
+  line(width*0.95, height*0.95,  mouseX,mouseY);
 }
