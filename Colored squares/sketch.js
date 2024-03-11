0500c69a3c1ac1c47a091dc5afce09aac96c48c8
ShamsSPSD <233007shams@gmail.com>
2024-03-11 15:21:24 -0600

    Coloured square
    done the grid

--- Colored squares/sketch.js
+++ Colored squares/sketch.js
@@ -5,11 +5,29 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let y = 0
+let squaresize = 20
+
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function squarmaker(){
+  for(let x = 0; x < width; x += squaresize){
+    for(let y = 0; y < height; y += squaresize){
+      square(x,y,squaresize)
+    }
+
+  }
+
+
+}
+
+
 function draw() {
-  background(220);
+  background(100);
+  squarmaker();
 }
