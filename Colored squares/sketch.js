818296ea7f1ee61afaaa429f8e8930d026d245a5
ShamsSPSD <233007shams@gmail.com>
2024-03-12 15:20:46 -0600

    made new colour config
    still need to change the mousepressed

--- Colored squares/sketch.js
+++ Colored squares/sketch.js
@@ -6,28 +6,42 @@
 // - describe what you did to take this project "above and beyond"
 
 let y = 0
-let squaresize = 20
+let squaresize = 50
 
-
-
+let Greenv;
+let Redv; 
+let Bluev;
+let squareColor;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  squarmaker();
+}
+
+function keyPressed(){
+  squarmaker();
+}
+
+function mousePressed(){
+  if 
 }
 
 function squarmaker(){
-  for(let x = 0; x < width; x += squaresize){
-    for(let y = 0; y < height; y += squaresize){
-      square(x,y,squaresize)
+    for(let x = 0; x < width; x += squaresize){
+      for(let y = 0; y < height; y += squaresize){
+        squareColor = color(Redv,Greenv,Bluev);
+        fill(squareColor)
+        square(x,y,squaresize)
+      
+        Greenv = random(0,255)
+        Bluev = random(0,255)
+        Redv = random(0,255)
+      }
+  
     }
-
-  }
-
-
-}
-
+  } 
+  
 
 function draw() {
-  background(100);
-  squarmaker();
+
 }
