759f33ce8a7083cb63883c2662c0ce97faea5868
ShamsSPSD <233007shams@gmail.com>
2024-03-18 15:24:43 -0600

    proj for clock
    added lines

---
+++ Transformation/sketch.js
@@ -0,0 +1,19 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function face(100,100){
+
+}
+
+function draw() {
+  background(220);
+}
---
+++ Transformations Demo/sketch.js
@@ -0,0 +1,71 @@
+// Basic Transformations Sandbox
+
+
+let originalSpacing = 20;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+  drawBasicGrid(150);
+  makecircle(height/2,width/2,50)
+  let angle = 0;
+  while(angle < 360){
+    push();
+    rotate(angle);
+    line(700,300,600,150)
+    pop();
+    angle += 1
+  }
+
+
+}
+
+
+function face(x, y) {
+  //draw a face at x,y
+  push();
+  translate(x,y);
+  ellipseMode(CENTER);
+  fill(200,200,0);
+  stroke(0);
+  ellipse(0,0,80,80);
+  fill(90, 140, 30, 220);
+  triangle(-20, 20, 20, 20, 0, 30);
+  fill(0);
+  ellipse(-25,0,10,10);
+  ellipse(25,0,10,10);
+  strokeWeight(5);
+  line(-30,-10,30,-10);
+  strokeWeight(1);
+  pop();
+
+}
+
+function makecircle(x,y){
+  fill(255);
+  circle(x,y,500);
+}
+
+
+
+function drawBasicGrid(shade) {
+  //draw the normal cartesian Coordinate Grid, in a light color. Spaced at 20 px by default
+  stroke(shade);
+  for (let x = 0; x < width; x += 20) {
+    line(x, 0, x, height);
+  }
+  for (let y = 0; y < height; y += 20) {
+    line(0, y, width, y);
+  }
+
+  //Draw "X" at the origin
+  strokeWeight(3);
+  stroke(0);
+  line(-5,0,5,0);
+  line(0,5,0,-5);
+  strokeWeight(1);
+}
