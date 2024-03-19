dda3cc76e390309e599885f4f82bdb3aebe9752c
ShamsSPSD <233007shams@gmail.com>
2024-03-19 15:20:50 -0600

    Started the perlin assignment
    Did the beginning

--- Perlin noise terrarian/sketch.js
+++ Perlin noise terrarian/sketch.js
@@ -3,13 +3,30 @@
 // Date
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Terrain Generation Starter
+// Mr. Scott
+// March 11, 2024
+// Procedurally Generated 2D Terrain
 
+let rectWidth = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255);
+  rectMode(CORNER); 
+  drawRectangles();
+}
+
+function drawRectangles(){
+  let rectHeight;
+  fill(0);
+  for(let x = 0; x < width; x += rectWidth){
+    rectHeight = random(0, height*-0.8);
+
+    rect(x, height, rectWidth, rectHeight);
+
+  }
 }
 
 function draw() {
-  background(220);
 }
