e631f42a9ce385e379416120ea315b5ce5c1a532
ShamsSPSD <233007shams@gmail.com>
2024-03-19 14:49:21 -0600

    used the clock hands
    Built it

--- Transformations Demo/sketch.js
+++ Transformations Demo/sketch.js
@@ -1,71 +1,46 @@
-// Basic Transformations Sandbox
-
-
-let originalSpacing = 20;
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(255);
-  drawBasicGrid(150);
-  makecircle(height/2,width/2,50)
-  let angle = 0;
-  while(angle < 360){
-    push();
-    rotate(angle);
-    line(700,300,600,150)
-    pop();
-    angle += 1
-  }
-
-
+  background(220);
+  drawStaticClock();
+  ClockHands();
 }
 
+function ClockHands(){
 
-function face(x, y) {
-  //draw a face at x,y
   push();
-  translate(x,y);
-  ellipseMode(CENTER);
-  fill(200,200,0);
-  stroke(0);
-  ellipse(0,0,80,80);
-  fill(90, 140, 30, 220);
-  triangle(-20, 20, 20, 20, 0, 30);
-  fill(0);
-  ellipse(-25,0,10,10);
-  ellipse(25,0,10,10);
-  strokeWeight(5);
-  line(-30,-10,30,-10);
+  stroke(200,0,0);
   strokeWeight(1);
+  rotate(second()*6);
+  line(0,0,0,130);
   pop();
 
 }
 
-function makecircle(x,y){
-  fill(255);
-  circle(x,y,500);
-}
 
-
-
-function drawBasicGrid(shade) {
-  //draw the normal cartesian Coordinate Grid, in a light color. Spaced at 20 px by default
-  stroke(shade);
-  for (let x = 0; x < width; x += 20) {
-    line(x, 0, x, height);
-  }
-  for (let y = 0; y < height; y += 20) {
-    line(0, y, width, y);
-  }
-
-  //Draw "X" at the origin
-  strokeWeight(3);
+function drawStaticClock(){
   stroke(0);
-  line(-5,0,5,0);
-  line(0,5,0,-5);
-  strokeWeight(1);
+  translate(width/2, height/2);
+  push(); 
+  circle(0,0,300);
+
+
+  let count = 0; let angle = 6;
+  while(count < 60){
+    if(count % 5 === 0){
+      strokeWeight(3);
+      line(110,0,140,0);
+    }
+    else{
+      strokeWeight(1);
+      line(125,0,140,0);
+    }
+   
+    rotate(angle);
+    count++;
+  }
+  pop();
 }
