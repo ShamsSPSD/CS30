0d4041fd7354d09aa77acd84983ebbba1ef0ea24
ShamsSPSD <233007shams@gmail.com>
2024-03-28 12:27:27 -0600

    no work done
    work in genart

--- Perlin noise terrarian/sketch.js
+++ Perlin noise terrarian/sketch.js
@@ -3,32 +3,47 @@
 // March 11, 2024
 // Procedurally Generated 2D Terrain
 
-let rectWidth = 5;
+let rectWidth = 1;
+let rectHeight = 0;
+let rectTime = 1;
+let currHP = 0;
+let xHP = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   //rectMode(CENTER);  //CHANGE THIS!!!!
   drawRectangles();
+  print(currHP);
+  print(xHP);
 }
 
 function drawRectangles(){
   //using a single loop, generate a bunch of side-to-side
   //rectangles of varying height (pattern, random, noise)
-  let rectHeight;
+  let timeX = rectTime;
+
   fill(0);
   for(let x = 0; x < width; x += rectWidth){
-    //option 1 - pattern
-    // rectHeight = x;
+    timeX += 0.005;
 
-    //option 2 - random()
-    rectHeight = noise(0,height* 0.8);
-
-    //perlin noise.. on your own.
+    rectHeight = map(noise(timeX),0,1,0,height * 0.8)
+    
     rect(x, height, rectWidth, -rectHeight);
-
+     if (currHP < rectHeight){
+        currHP = rectHeight
+        xHP = x
   }
+    
+  }
+  drawflag(xHP,height-currHP);
 }
 
-function draw() {
+
+function drawflag(x,y){
+  fill(120,0,60);
+ circle(x,y,30)
 }
+
+
