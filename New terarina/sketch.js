b01e73f4a3cf606efd3dfd52e7d81b4437453afc
ShamsSPSD <233007shams@gmail.com>
2024-04-16 14:35:28 -0600

    Done with perlin noise project
    Added comments and finished all challenges

--- New terarina/sketch.js
+++ New terarina/sketch.js
@@ -1,21 +1,20 @@
-// Terrain Generation Starter
-// Mr. Scott
-// March 11, 2024
-// Procedurally Generated 2D Terrain
+// Terrain Generation Perlin noise
+// Omar Shams
+// April 16, 2024
 
+//defining all variables
 let rectWidth = 1;
 let rectHeight = 0;
 let rectTime = 1;
 let currHP = 0;
 let xHP = 0;
 let avheight = 0;
-let reccount = 0;
 
-function draw(){
+function draw(){//beginning the draw function
     background(255);
-    rectTime += 0.03;
+    rectTime += .03;//keeps generating terrain and determines the speed
     drawRectangles();
-    
+    //controls the thickness of the rectangles and limits how big or small they can be
     if (keyIsPressed && keyCode === LEFT_ARROW) {
         rectWidth -= 0.1;    
     } 
@@ -33,21 +32,20 @@ function draw(){
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
-
 function drawRectangles(){
+    //more variable definitions
     let timeX = rectTime;
     reccount = 0;
     avheight = 0;
     currHP = 0;
-    
     fill(255);
+    //initial for loop that draws the rectangles and randomizes with perlin noise 
     for(let x = 0; x < width; x += rectWidth){
         timeX += 0.005;
         rectHeight = map(noise(timeX), 0, 1, 0, height * 0.8);
-        
         reccount++;
         avheight += rectHeight;
-        
+        //determines the highest point and defines where the flag is
         rect(x, height, rectWidth, -rectHeight);
         if (currHP < rectHeight){
             currHP = rectHeight;
@@ -57,20 +55,20 @@ function drawRectangles(){
     
     avheight = avheight / reccount; // Calculate average height
     
-    // Draw average line
+    // Draw average line and dictates its location
     avgline(height - avheight);
     
     // Draw flag at the highest point
     drawflag(xHP, height - currHP - 10);
 }
-
+//the flag itself
 function drawflag(x, y){
     fill(120, 0, 60);
     line(x, y, x, y + 12.5);
     fill(200);
     triangle(x, y, x, y - 13, x + 15, y - 6);
 }
-
+//the avarage line
 function avgline(average){
     fill(255, 0, 0);
     rect(0, average, width, 2); // Draw a single red line representing the average height
