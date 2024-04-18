c1f276efe61854ab2f5e9b2821f89cb73af6ccc3
ShamsSPSD <233007shams@gmail.com>
2024-04-18 15:12:36 -0600

    Finished generated art
    not many comments

--- Genartporj#1/sketch.js
+++ Genartporj#1/sketch.js
@@ -1,21 +1,22 @@
 // “Ninety Parallel Sinusoids With Linearly Increasing Period”
 // Omar Shams
-// April 16 2024
-//
-// “Ninety Parallel Sinusoids With Linearly Increasing Period”
+// April 18 2024
 
 function setup() {
   createCanvas(500  , 500);
   background(220);
   
+
+  //replicates the sine wave and moves it downwards 
   for (let i = 0; i < 200; i += 5){    
     push();
-    translate(75,height/3 + i);
+    translate(75,height/3 + i );
     theWave();
     pop();
   }
 }
 
+//makes the wave 
 function theWave(){
   let x = 0;
   noFill();
