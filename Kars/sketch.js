22599d19fd70a825649795348dfb5b4562bc6fb1
ShamsSPSD <233007shams@gmail.com>
2024-05-14 15:23:55 -0600

    New design for cars
    will finish tom

--- Kars/sketch.js
+++ Kars/sketch.js
@@ -55,14 +55,8 @@ class Kars{
 
   }
   truck(){
-    rect(this.x, this.y, 30,60);
-    fill(random(1,255));
-    rect(this.x, this.y, 30,30);
-    fill(random(1,255));
-    rect(length/2, + 5,width/2, - 5, 5,5)
-    rect(length/2, - 5,width/2, - 5, 5,5)
-    rect(length/2, + 5,width/2, + 5, 5,5)
-    rect(length/2, + 5,width/2, + 5, 5,5)
+    
+  
   }
 }
 
