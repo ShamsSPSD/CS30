7c56a05ebab9ef84a0f1bc0572e1dddec1a26437
ShamsSPSD <160178531+ShamsSPSD@users.noreply.github.com>
2024-05-28 15:24:05 -0600

    simplirified the zomb
    Will make map

--- Capestone ZombJs/sketch.js
+++ Capestone ZombJs/sketch.js
@@ -4,7 +4,7 @@
 let player;
 let zombies = [];
 
-let framesTillCreate = 300;
+let framesTillCreate = 100;
 let frame = 0;
 let speed = 2;
 let score = 0;
@@ -13,14 +13,12 @@ function setup() {
   createCanvas(700, 700);
   imageMode(CENTER);
   player = new Player();
-  zombieImg = loadImage("zombie.png");
-  playerImg = loadImage("player.png");
-  grassImg = loadImage("grass.jpg");
   zombies.push(new Zombie(random(speed)));
 }
 
 function draw() {
-  image(grassImg, 0, 0, width*2, height * 2);
+
+  rect(width/2,height/2,width,height)
   
   frame++;
   player.draw();
@@ -46,9 +44,7 @@ function draw() {
   if (frameCount % 1000 == 0) {
     speed+=0.1;
   }
-  textAlign(CENTER);
-  textSize(40);
-  text(score, width/2, 100);
+  
   
 }
 
@@ -56,4 +52,135 @@ function draw() {
 
 function mouseClicked() {
   player.shoot();
+}
+
+class Bullet {
+  constructor(x, y, angle) {
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+    this.speed = 16;
+  }
+  
+  
+  draw() {
+    push();
+    fill(0);
+    circle(this.x, this.y, 5);
+    pop();
+  }
+  
+  update() {
+    this.x += this.speed * cos(this.angle);
+    this.y += this.speed * sin(this.angle);
+  }
+}
+
+class Player {
+  constructor() {
+    this.pos = createVector(width/2, height/2);
+    this.bullets = [];
+    this.angle = 0;
+  }
+
+  draw() {
+    rectMode(CENTER);
+    push();
+    translate(this.pos.x, this.pos.y);
+    this.angle = atan2(mouseY - this.pos.y, mouseX - this.pos.x);
+    rotate(this.angle);
+    rect(0, 0, 10, 10);
+    pop();
+    
+    
+    for (let bullet of this.bullets) {
+      bullet.draw();
+      bullet.update();
+    }
+    
+    if (this.bullets.length > 20) {
+      this.bullets.splice(0, 1);
+    }
+  }
+
+  update() {
+    
+    let sidewaysSpeed = 0;
+    let forwardSpeed = 0;
+    if (keyIsDown(65)) {
+      sidewaysSpeed = -2;
+    }
+
+    if (keyIsDown(68)) {
+      sidewaysSpeed = 2;
+    }
+
+    if (keyIsDown(87)) {
+      forwardSpeed = -2;
+    }
+
+    if (keyIsDown(83)) {
+      forwardSpeed = 2;
+    }
+    
+    this.pos.add(sidewaysSpeed, forwardSpeed);
+  }
+  
+  shot(zombie) {
+    for (let i = this.bullets.length - 1; i >= 0; i--) {
+      if (dist(this.bullets[i].x, this.bullets[i].y, zombie.pos.x, zombie.pos.y) < 10) {
+        this.bullets.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  shoot() {
+    this.bullets.push(new Bullet(this.pos.x, this.pos.y, this.angle));
+  }
+}
+
+class Zombie {
+  
+  constructor(speed) {
+    this.x = random(width);
+    this.y = random(height);
+    
+    if (random(1) > 0.5) {
+      this.x += width;
+    } else {
+      this.x -= width;
+    }
+    
+    if (random(1) > 0.5) {
+      this.y += height;
+    } else {
+      this.y -= height;
+    }
+    this.pos = createVector(this.x, this.y);
+    this.speed = speed;
+    this.angle = 0;
+  }
+  
+  
+  draw() {
+    angleMode(degrees);
+    rectMode(CENTER);
+    push();
+    translate(this.pos.x, this.pos.y);
+    this.angle = atan2(player.pos.y - this.pos.y, player.pos.x - this.pos.x);
+    rotate(this.angle);
+    fill(100, 255, 100);
+    rect( 0, 0, 20, 20);
+    //rect(0, 0, 20, 20);
+    pop();
+  }
+  
+  update() {
+    let difference = p5.Vector.sub(player.pos, this.pos);
+    difference.limit(this.speed);
+    this.pos.add(difference);
+  }
+  
 }
