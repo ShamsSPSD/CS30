611c7cb8693e2d5fd891ec491fff9a1ce828b05a
ShamsSPSD <160178531+ShamsSPSD@users.noreply.github.com>
2024-06-10 14:44:45 -0600

    fixed commit issues
    work on lucky chest

--- Capestone ZombJs/sketch.js
+++ Capestone ZombJs/sketch.js
@@ -32,7 +32,6 @@ function setup() {
   player = new Player();
   zombies.push(new Zombie(3));
 }
-
 function draw() {
   background(255);
 
@@ -111,13 +110,10 @@ function drawGameScreen() {
   if (playerHealth <= 0) {
     Modeselect = 2;
   }
-
   translate(width / 2 - player.pos.x, height / 2 - player.pos.y);
   drawGrid();
-
   player.display();
   player.update();
-
   for (let i = zombies.length - 1; i >= 0; i--) {
     zombies[i].display();
     zombies[i].update();
@@ -149,18 +145,15 @@ function drawGameScreen() {
     zombies.push(new Zombie(random(speed)));
     frame = 0;
   }
-
   if (framesTillCreate > 20) {
     framesTillCreate *= 0.95;
     if (framesTillCreate < 20) {
       framesTillCreate = 20;
     }
   }
-
   if (frameCount % 1000 == 0) {
     speed += 0.1;
   }
-
   drawHealthMeter();
   drawScore();
 
@@ -182,7 +175,6 @@ function drawGameOverScreen() {
     restartGame();
   }
 }
-
 function drawGrid() {
   for (let x = 0; x <= mapSize; x += gridSize) {
     for (let y = 0; y <= mapSize; y += gridSize) {
@@ -191,19 +183,16 @@ function drawGrid() {
     }
   }
 }
-
 function drawHealthMeter() {
   fill(255, 0, 0);
   rect(player.pos.x - width / 2 + 350, player.pos.y - height / 2 + 320, playerHealth / 2, 10);
 }
-
 function drawScore() {
   fill(0);
   textAlign(LEFT);
   textSize(16);
   text("Score: " + score, player.pos.x - width / 2 + 10, player.pos.y - height / 2 + 30);
 }
-
 function restartGame() {
   playerHealth = 100;
   score = 0;
@@ -215,7 +204,6 @@ function restartGame() {
   player.pos = createVector(mapSize / 2, mapSize / 2);
   Modeselect = 1;
 }
-
 class Bullet {
   constructor(x, y, angle) {
     this.x = x;
@@ -223,27 +211,23 @@ class Bullet {
     this.angle = angle;
     this.speed = 16;
   }
-
   display() {
     push();
     fill(0);
     circle(this.x, this.y, 5);
     pop();
   }
-
   update() {
     this.x += this.speed * cos(this.angle);
     this.y += this.speed * sin(this.angle);
   }
 }
-
 class Player {
   constructor() {
     this.pos = createVector(mapSize / 2, mapSize / 2);
     this.bullets = [];
     this.angle = 0;
   }
-
   display() {
     rectMode(CENTER);
     push();
@@ -252,37 +236,30 @@ class Player {
     rotate(this.angle);
     rect(0, 0, 10, 10);
     pop();
-
     for (let bullet of this.bullets) {
       bullet.display();
       bullet.update();
     }
   }
-
   update() {
     let sidewaysSpeed = 0;
     let forwardSpeed = 0;
     if (keyIsDown(65)) {
       sidewaysSpeed = -5;
     }
-
     if (keyIsDown(68)) {
       sidewaysSpeed = 5;
     }
-
     if (keyIsDown(87)) {
       forwardSpeed = -5;
     }
-
     if (keyIsDown(83)) {
       forwardSpeed = 5;
     }
-
     this.pos.add(sidewaysSpeed, forwardSpeed);
     this.pos.x = constrain(this.pos.x, 0, mapSize);
     this.pos.y = constrain(this.pos.y, 0, mapSize);
   }
-
   shot(zombie) {
     for (let i = this.bullets.length - 1; i >= 0; i--) {
       if (dist(this.bullets[i].x, this.bullets[i].y, zombie.pos.x, zombie.pos.y) < 10) {
@@ -292,7 +269,6 @@ class Player {
     }
     return false;
   }
-
   shoot() {
     this.bullets.push(new Bullet(this.pos.x, this.pos.y, this.angle));
   }
@@ -307,7 +283,6 @@ class Player {
     this.pos.add(direction.mult(7));
   }
 }
-
 class Zombie {
   constructor(speed) {
     this.speed = speed;
@@ -338,7 +313,6 @@ class Zombie {
     }
     this.pos = createVector(this.x, this.y);
   }
-
   display() {
     angleMode(DEGREES);
     rectMode(CENTER);
@@ -349,19 +323,16 @@ class Zombie {
     fill(100, 255, 100);
     rect(0, 0, 20, 20);
     pop();
-
     fill(255, 0, 0);
     rect(this.pos.x - 10, this.pos.y - 20, 20, 3);
     fill(0, 255, 0);
     rect(this.pos.x - 10, this.pos.y - 20, this.health / 5, 3);
   }
-
   update() {
     let difference = p5.Vector.sub(player.pos, this.pos);
     difference.limit(this.speed);
     this.pos.add(difference);
   }
-
   pushBack(player) {
     let direction = p5.Vector.sub(this.pos, player.pos);
     direction.normalize();
